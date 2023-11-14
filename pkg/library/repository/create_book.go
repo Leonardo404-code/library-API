4846de1328a063a0cc419c3ea3e5ba3cda91f9d8
Leonardo Bispo <leonardobispo1000@gmail.com>
2023-11-14 12:26:30 -0300

    fix: fixed validation and added gender field to book creation

--- pkg/library/repository/create_book.go
+++ pkg/library/repository/create_book.go
@@ -3,16 +3,17 @@ package repository
 import (
 	"context"
 
-	"library-api/pkg/library"
-
 	"go.mongodb.org/mongo-driver/bson"
+
+	"library-api/pkg/library"
 )
 
 func (r *repository) CreateBook(book *library.Book) error {
 	_, err := r.conn.InsertOne(context.Background(), bson.D{
-		{Key: "name", Value: book.Name},
+		{Key: "title", Value: book.Title},
 		{Key: "description", Value: book.Description},
 		{Key: "writer", Value: book.Writer},
+		{Key: "gender", Value: book.Gender},
 		{Key: "release_date", Value: book.ReleaseDate},
 		{Key: "created_at", Value: book.CreatedAt},
 		{Key: "updated_at", Value: book.UpdatedAt},
