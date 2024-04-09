d38b1eaa061b04b7eb5e385b50f05b043d9cb75f
Leonardo Bispo <leonardobispo1000@gmail.com>
2024-04-09 15:49:50 -0300

    refactor: removed deadcode

--- internal/env/env.go
+++ internal/env/env.go
@@ -23,74 +23,12 @@ func GetOrDefault(envName, defaultValue string) string {
 	return value
 }
 
-func GetOrDefaultInt(envName string, defaultValue int) int {
-	value := viper.GetInt(envName)
-
-	if !viper.IsSet(envName) {
-		return defaultValue
-	}
-
-	return value
-}
-
-func GetOrDefaultInt64(envName string, defaultValue int64) int64 {
-	value := viper.GetInt64(envName)
-
-	if !viper.IsSet(envName) {
-		return defaultValue
-	}
-
-	return value
-}
-
-func GetOrDefaultFloat64(envName string, defaultValue float64) float64 {
-	value := viper.GetFloat64(envName)
-
-	if !viper.IsSet(envName) {
-		return defaultValue
-	}
-
-	return value
-}
-
-func GetStringSlice(envName string) []string {
-	if !viper.IsSet(envName) {
-		panic(fmt.Sprintf("env variable %s is not set", envName))
-	}
-
-	return viper.GetStringSlice(envName)
-}
-
 func GetString(envName string) string {
 	if !viper.IsSet(envName) {
 		panic(fmt.Sprintf("env variable %s is not set", envName))
 	}
 
 	return viper.GetString(envName)
-}
-
-func GetStringMap(envName string) map[string]string {
-	if !viper.IsSet(envName) {
-		panic(fmt.Sprintf("env variable %s is not set", envName))
-	}
-
-	return viper.GetStringMapString(envName)
-}
-
-func GetInt(envName string) int {
-	if !viper.IsSet(envName) {
-		panic(fmt.Sprintf("env variable %s is not set", envName))
-	}
-
-	return viper.GetInt(envName)
-}
-
-func GetFloat64(envName string) float64 {
-	if !viper.IsSet(envName) {
-		panic(fmt.Sprintf("env variable %s is not set", envName))
-	}
-
-	return viper.GetFloat64(envName)
 }
 
 func Load() {
